84e74f18c4f514173e7002edbcb87e76850d0b9a
NintiTan <145343837+NintiTan@users.noreply.github.com>
2024-11-27 11:55:09 +0100

    pagina 1 zo goed als af

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
-console.log("hi");
+
+
+// Image animation
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hiddenElements = document.querySelectorAll(".hidden");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, { threshold: 0.3 }); 
+
+    hiddenElements.forEach(el => observer.observe(el));
+  });
