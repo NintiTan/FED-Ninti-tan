5d2c87a90757c642e0a9b31bafdc74409a26beff
NintiTan <145343837+NintiTan@users.noreply.github.com>
2024-12-01 17:20:26 +0100

    latest stories vormgeving

--- scripts/script.js
+++ scripts/script.js
@@ -23,12 +23,26 @@ let currentImage = 0;
 
 // product carousel
 
+const captions = [
+  { p1: "Synched Fligh Small Smolder Blue", p2: "1/3" },
+  { p1: "Model is 176cm wearing a size small", p2: "2/3" },
+  { p1: "Model is 176cm wearing a size small", p2: "3/3" },
+];
+
 function showImage(current) {
   const carousel = document.querySelector('div > div');
   const totalImages = carousel.children.length;
   currentImage = (current + totalImages) % totalImages;
   carousel.style.transform = `TranslateX(-${currentImage * 100}%)`;
+
+  const p1 = document.querySelector('#detailpage div p:first-of-type');
+  const p2 = document.querySelector('#detailpage div  p:nth-of-type(2)');
+
+  p1.textContent = captions[currentImage].p1;
+  p2.textContent = captions[currentImage].p2
 }
+
+
 
 function nextImage() {
   showImage(currentImage + 1);
